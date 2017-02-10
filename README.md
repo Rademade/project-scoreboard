225b02ae2052af2cd7b0ea09cf19a2dc0a67644c
kach95 <paliukh.mykhailo@domain.com>
2017-02-10 21:18:20 +0200

    init new rails app scoreboard --api

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rademade Scoreboard
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Scoreboard
+  class Application < Rails::Application
+    config.api_only = true
+  end
+end
