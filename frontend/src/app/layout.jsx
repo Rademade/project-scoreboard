b428664d6dd9c0c8ff6d1a2b698b616c6b328a5b
kach95 <paliukh.mykhailo@domain.com>
2017-03-08 22:48:00 +0200

    refactor

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -4,7 +4,6 @@ import {Provider} from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import {grey500} from 'material-ui/styles/colors'
 import Header from 'components/Header'
 import routes from './routes'
 import store from './store'
@@ -14,8 +13,7 @@ injectTapEventPlugin()
 
 const muiTheme = getMuiTheme({
   appBar: {
-    height: 55,
-    color: grey500
+    height: 55
   },
 })
 
---
+++ lib/services/jira/serializers/issues.rb
@@ -0,0 +1,35 @@
+module Services
+  module Jira
+    module Serializers
+      class Issues
+        attr_reader :issues, :story_points_field
+
+        def initialize(issues, story_points_field)
+          @issues = issues
+          @story_points_field = story_points_field
+        end
+
+        def serialize
+          Parallel.map(issues, in_threads: 2) do |issue|
+            serialize_issue(issue['fields'])
+          end.compact
+        end
+
+        private
+
+        def serialize_issue(issue)
+          if story_points = issue[story_points_field]
+            {
+              updated: issue['updated'],
+              resolution_date: issue['resolutiondate'],
+              status: issue.dig('resolution', 'name'),
+              summary: issue['summary'],
+              story_points: story_points
+            }
+          end
+        end
+
+      end
+    end
+  end
+end
