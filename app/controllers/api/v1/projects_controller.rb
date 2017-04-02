9f5466704e90d55b166af22c278b9a0000d82fc6
95kach <paliukh.mykhailo@gmail.com>
2017-04-02 16:54:16 +0300

    added model scopes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProjectsController < Api::ApplicationController
   def index
-    render json: Project.select(:id, :name)
+    render json: Project.with_tuned_jira.select(:id, :name)
   end
 
   def show
