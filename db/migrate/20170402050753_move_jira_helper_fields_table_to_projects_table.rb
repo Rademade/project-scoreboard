9ed58a868818723ade0d63669fe6a78d300ac62b
95kach <paliukh.mykhailo@gmail.com>
2017-04-02 16:40:35 +0300

    remove null constraints

--- db/migrate/20170402050753_move_jira_helper_fields_table_to_projects_table.rb
+++ db/migrate/20170402050753_move_jira_helper_fields_table_to_projects_table.rb
@@ -1,8 +1,8 @@
 class MoveJiraHelperFieldsTableToProjectsTable < ActiveRecord::Migration[5.0]
   def change
     remove_column :projects, :jira_helper_field_id
-    add_column :projects, :jira_rapid_view_id, :integer, null: false
-    add_column :projects, :jira_story_points_field, :string, null: false, default: 'customfield_10022'
+    add_column :projects, :jira_rapid_view_id, :integer
+    add_column :projects, :jira_story_points_field, :string, default: 'customfield_10022'
     drop_table :jira_helper_fields
   end
 end
